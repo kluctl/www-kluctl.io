f5ce4f3e71a48e2fb62ef341c05b8a38c59f65b0
Alexander Block <ablock84@gmail.com>
2022-04-06 15:44:16 +0200

    Use fullversion from config.toml in build.sh

--- build.sh
+++ build.sh
@@ -10,12 +10,12 @@ case "$(uname -s)" in
       *)          echo "unknown os"; exit 1;
 esac
 
-echo "Determining latest_version"
-latest_version=$(curl -s https://api.github.com/repos/kluctl/kluctl/releases/latest | jq '.tag_name' -r)
-echo "latest_version=$latest_version"
+echo "Determining version"
+version=$(cat config.toml | grep 'fullversion =' | sed 's/fullversion = "\(.*\)"/\1/')
+echo "version=$version"
 
 mkdir -p /tmp/kluctl-for-docs
-curl -L -o /tmp/kluctl-for-docs/kluctl$exe "https://github.com/kluctl/kluctl/releases/download/$latest_version/kluctl-$arch$exe"
+curl -L -o /tmp/kluctl-for-docs/kluctl$exe "https://github.com/kluctl/kluctl/releases/download/$version/kluctl-$arch$exe"
 chmod +x /tmp/kluctl-for-docs/kluctl$exe
 ls -lah /tmp/kluctl-for-docs/
 
